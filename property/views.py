73240f92d1da9b9f86e17d94562f174877c1ca62
Marc Boanas <marcjohnboanas@gmail.com>
2016-04-29 10:24:42 +0100

    html update

--- property/views.py
+++ property/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Listing
 
 def property_list(request):
-	return render(request, 'property/property_list.html', {})
+	listings = Listing.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'property/property_list.html', {'listings': listings})
